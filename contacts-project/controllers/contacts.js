5101bbcbf6e3dc727af685f8de5b056cb311b7ee
Kopano Damane <kopano@penguinprotocols.com>
2026-05-21 21:08:27 +0200

    Completed W02 Project: Contacts Part 2

--- contacts-project/controllers/contacts.js
+++ contacts-project/controllers/contacts.js
@@ -1,6 +1,7 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
+// GET ALL
 const getAll = async (req, res) => {
   try {
     const result = await mongodb.getDb().collection('contacts').find();
@@ -13,6 +14,7 @@ const getAll = async (req, res) => {
   }
 };
 
+// GET SINGLE
 const getSingle = async (req, res) => {
   try {
     const contactId = new ObjectId(req.params.id);
@@ -20,12 +22,92 @@ const getSingle = async (req, res) => {
     const result = await mongodb
       .getDb()
       .collection('contacts')
-      .find({ _id: contactId });
-
-    const contact = await result.toArray();
+      .findOne({ _id: contactId });
 
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(contact[0]);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// CREATE CONTACT
+const createContact = async (req, res) => {
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const response = await mongodb
+      .getDb()
+      .collection('contacts')
+      .insertOne(contact);
+
+    if (response.acknowledged) {
+      res.status(201).json({
+        message: 'Contact created successfully',
+        id: response.insertedId
+      });
+    } else {
+      res.status(500).json(response.error || 'Error creating contact');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// UPDATE CONTACT
+const updateContact = async (req, res) => {
+  try {
+    const contactId = new ObjectId(req.params.id);
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const response = await mongodb
+      .getDb()
+      .collection('contacts')
+      .updateOne(
+        { _id: contactId },
+        { $set: contact }
+      );
+
+    if (response.matchedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({
+        message: 'Contact not found'
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// DELETE CONTACT
+const deleteContact = async (req, res) => {
+  try {
+    const contactId = new ObjectId(req.params.id);
+
+    const response = await mongodb
+      .getDb()
+      .collection('contacts')
+      .deleteOne({ _id: contactId });
+
+    if (response.deletedCount > 0) {
+      res.status(200).send();
+    } else {
+      res.status(500).json(response.error || 'Error deleting contact');
+    }
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -33,5 +115,8 @@ const getSingle = async (req, res) => {
 
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
 };
--- contacts-project/server.js
+++ contacts-project/server.js
@@ -3,10 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const mongodb = require('./data/database');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
