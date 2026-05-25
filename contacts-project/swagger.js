85e8c0effb8f261e6d0356ed66c7082147814679
Kopano Damane <kopano@penguinprotocols.com>
2026-05-25 20:44:36 +0200

    Fix Swagger configuration

--- contacts-project/swagger.js
+++ contacts-project/swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Contacts API',
     description: 'Contacts API Documentation'
   },
-  host: 'https://contacts-project-api-bcrq.onrender.com/',
-  schemes: ['http']
+  host: 'contacts-project-api-bcrq.onrender.com',
+  schemes: ['https']
 };
 
 const outputFile = './swagger-output.json';
